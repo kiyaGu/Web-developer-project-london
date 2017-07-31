06a4538f8d568c2f7baf3cce7c8f83d49f5fa2bd
KiyaG <hamilee.kiya@gmail.com>
2017-08-01 00:17:07 +0100

    created news feeds from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,37 +9,192 @@ var setElementAttribute = function(element, attribute, value) {
     element.setAttribute(attribute, value);
 };
 
+var modal = document.getElementById('newsDetail');
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//==========================ajax request===================================//
+  function Ajax_request(){
+  if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+      xmlhttp = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject) { // IE
+      try {
+        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+           }
+      catch (e) {
+        try {
+          xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        catch (e) {}
+      }//catch
+    }//else if
+  if (!xmlhttp) {
+      alert('Giving up : Cannot create an XMLHTTP instance');
+    }
+     return xmlhttp;
+  }
+var request = Ajax_request();
+(function(){
+  request.onreadystatechange = function() {
+   if (request.readyState === 4) {  // check if a response was sent back
+     if (request.status === 200) {     // check if request was successful
+       let news = JSON.parse(request.responseText);
+       var newsLength = news.length;
+       var readMoreFunc = [];
+       let newsSection = document.querySelector('#newsSection');
+       for(var i = 0; i < newsLength; i++){
+         let section = createElementFunc('section');
+         setElementAttribute(section,'id','news' + i);
+         let heading = createElementFunc('h2');
+         heading.appendChild(createTextContentFunc(news[i].title));
+         let image = createElementFunc('img');
+         if(news[i].image){
+           image.setAttribute('src', news[i].image);
+         } else{
+           image.setAttribute('src', 'assets/images/18.jpg');
+         }
+         var newsSnippet = news[i].body.substring(0, 150);
+         let content = createElementFunc('p');
+         content.appendChild(createTextContentFunc(newsSnippet + " ................ "));
+         let button = createElementFunc('button');
+         setElementAttribute(button, 'class', 'readMore');
+         setElementAttribute(button, 'id', 'btnNews' + i);
+         button.appendChild(createTextContentFunc('More'));
+         setElementAttribute(button, 'onclick','readMoreNews(' + i + ')');
+         section.appendChild(heading);
+         section.appendChild(image);
+         section.appendChild(content);
+         section.appendChild(button);
+         newsSection.appendChild(section);
+       }
+     } else {
+       alert('An error occurred during your request: ' +  request.status + ' ' + request.statusText);
+     }
+   }
+   }
+  var url = 'http://cyf-api.herokuapp.com/news';                                        //server location
+  request.open('GET', url);                    // adding it to the request
+  request.setRequestHeader('Accepts', 'application/json'); //header info
+  request.send();
+})();
+
 /*
 for hidding and showing the detailed news in the news section and to show it only when the
 user clicks 'more' button. It also used for the reverse to hide the detailed news when the
 user click 'less' button.
 */
-var readMore = function(btn){
+var readMore = function(btn,heading,img,content){
   var newsDetailProp = window.getComputedStyle(document.querySelector('#newsDetail'),null);
   if(newsDetailProp.getPropertyValue("display") === 'none'){
     //to display the detailed content of the news when the button 'more' is clicked
-    document.querySelector('#newsDetail').style.display = "block";
-    btn.innerHTML = 'Less';
+
+     let container = document.querySelector("#newsDetailContent");
+     if(img){
+       document.querySelector('#newsDetailContent img').setAttribute("src",img);
+     } else{
+       document.querySelector('#newsDetailContent img').setAttribute("src", 'assets/images/18.jpg');
+     }
+
+
+     document.querySelector('#newsDetail').style.display = "block";
+     document.querySelector('#newsDetailContentHeading').innerHTML = heading;
+     document.querySelector('.newsDetailContentP').innerHTML = content;
   } else{
     //to display less content 'higlight' of the news when the button 'less' is clicked
     document.querySelector('#newsDetail').style.display = "none";
-    btn.innerHTML = 'More';
+    // btn.innerHTML = 'More';
   }
 }
 
-//for each read 'More' button
-var btnReadMoreNews1 = document.querySelector('#btnNews1');
-btnReadMoreNews1.addEventListener('click', function(){
-  readMore(this);
-});
-var btnReadMoreNews2 = document.querySelector('#btnNews2');
-btnReadMoreNews2.addEventListener('click', function(){
-  readMore(this);
-});
-var btnReadMoreNews3 = document.querySelector('#btnNews3');
-btnReadMoreNews3.addEventListener('click', function(){
-  readMore(this);
-});
+var readMoreNews = function(i){
+request.onreadystatechange = function() {
+ if (request.readyState === 4) {  // check if a response was sent back
+   if (request.status === 200) {     // check if request was successful
+     //console.log(JSON.parse(request.responseText)[0].body);
+     document.querySelector('#newsDetail').style.display = "block";
+     let response = JSON.parse(request.responseText);
+     document.querySelector('#newsDetailContentHeading').innerHTML = response[i].title;
+     document.querySelector('.newsDetailContentP').innerHTML = response[i].body;
+     if(response[i].image){
+       document.querySelector('#newsDetailContent img').setAttribute("src",response[i].image);
+     } else{
+       document.querySelector('#newsDetailContent img').setAttribute("src", 'assets/images/18.jpg');
+     }
+
+     //readMore(this,heading,img,content);
+   } else {
+     alert('An error occurred during your request: ' +  request.status + ' ' + request.statusText);
+   }
+ }
+ }
+var url = 'http://cyf-api.herokuapp.com/news';                                        //server location
+request.open('GET', url);                    // adding it to the request
+request.setRequestHeader('Accepts', 'application/json'); //header info
+request.send();                                 // sending the reque
+
+};
+
+
+
+
+
+
+
+// //for each read 'More' button
+// var btnReadMoreNews1 = document.querySelector('#btnNews1');
+//
+//
+//
+//
+// var btnReadMoreNews2 = document.querySelector('#btnNews2');
+// btnReadMoreNews2.addEventListener('click', function(){
+//   request.onreadystatechange = function() {
+//  if (request.readyState === 4) {  // check if a response was sent back
+//    if (request.status === 200) {     // check if request was successful
+//      //console.log(JSON.parse(request.responseText)[0].body);
+//      let heading = JSON.parse(request.responseText)[1].title;
+//      let img = JSON.parse(request.responseText)[1].image;
+//      let content = JSON.parse(request.responseText)[1].body;
+//      readMore(this,heading,img,content);
+//    } else {
+//      alert('An error occurred during your request: ' +  request.status + ' ' + request.statusText);
+//    }
+//  }
+//  }
+// var url = 'http://cyf-api.herokuapp.com/news';                                        //server location
+// request.open('GET', url);                    // adding it to the request
+// request.setRequestHeader('Accepts', 'application/json'); //header info
+// request.send();
+// });
+// var btnReadMoreNews3 = document.querySelector('#btnNews3');
+// btnReadMoreNews3.addEventListener('click', function(){
+//   request.onreadystatechange = function() {
+//  if (request.readyState === 4) {  // check if a response was sent back
+//    if (request.status === 200) {     // check if request was successful
+//      //console.log(JSON.parse(request.responseText)[0].body);
+//      let heading = JSON.parse(request.responseText)[2].title;
+//      let img = JSON.parse(request.responseText)[2].image;
+//      let content = JSON.parse(request.responseText)[2].body;
+//      readMore(this,heading,img,content);
+//    } else {
+//      alert('An error occurred during your request: ' +  request.status + ' ' + request.statusText);
+//    }
+//  }
+//  }
+// var url = 'http://cyf-api.herokuapp.com/news';                                        //server location
+// request.open('GET', url);                    // adding it to the request
+// request.setRequestHeader('Accepts', 'application/json'); //header info
+// request.send();
+// });
 
 //for validating the form
 var btnSubmit = document.querySelector('#submit');
